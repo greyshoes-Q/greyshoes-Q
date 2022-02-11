8ebd07e32075ba1b8359b0fb6ef60bb1b2a56da0
greyshoes-Q <greyshoesq@gmail.com>
2022-02-11 17:56:07 +0800

    update

--- assets/js/bookpicture.js
+++ assets/js/bookpicture.js
@@ -1,26 +1,26 @@
 var shaizi = document.getElementById("shaizi");
 shaizi.addEventListener("mouseover", function(){
-    this.src ="blog/第一篇/picture/8716209_R2.png";
+    this.src ="../blog/第1篇/picture/8716209_R2.png";
 })
 
 shaizi.addEventListener("mouseout", function(){
-    this.src ="blog/第一篇/picture/8716209_R.jpg";
+    this.src ="../blog/第1篇/picture/8716209_R.jpg";
 })
 
 var shizuo = document.getElementById("shizuo");
 shizuo.addEventListener("mouseover", function(){
-    this.src ="blog/第一篇/picture/getImage2.jpg";
+    this.src ="../blog/第1篇/picture/getImage2.jpg";
 })
 
 shizuo.addEventListener("mouseout", function(){
-    this.src ="blog/第一篇/picture/getImage.jpg";
+    this.src ="../blog/第1篇/picture/getImage.jpg";
 })
 
 var qc = document.getElementById("qc");
 qc.addEventListener("mouseover", function(){
-    this.src ="blog/第一篇/picture/R2.jpg";
+    this.src ="../blog/第1篇/picture/R2.jpg";
 })
 
 qc.addEventListener("mouseout", function(){
-    this.src ="blog/第一篇/picture/R.jpg";
+    this.src ="../blog/第1篇/picture/R.jpg";
 })
