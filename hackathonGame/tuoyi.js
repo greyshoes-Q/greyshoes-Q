3b3ee40f64888fe44589ec746ef6e3946cdf2c3d
greyshoes-Q <greyshoesq@gmail.com>
2022-02-23 00:38:38 +0800

    update

--- hackathonGame/tuoyi.js
+++ hackathonGame/tuoyi.js
@@ -8,5 +8,4 @@ function Drop(event){
     event.preventDefault();
     var data=event.dataTransfer.getData("text");
     event.currentTarget.appendChild(document.getElementById(data));
-    prompt("Which circuit do you want this gate to be on (0 or 1)")
 }
