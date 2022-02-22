ff3b7d8b1c43d46806061a0a2be63fd830b08d16
greyshoes-Q <greyshoesq@gmail.com>
2022-02-22 22:43:58 +0800

    update

---
+++ hackathonGame/tuoyi.js
@@ -0,0 +1,11 @@
+function AllowDrop(event){
+    event.preventDefault();
+}
+function Drag(event){
+    event.dataTransfer.setData("text",event.currentTarget.id);
+}
+function Drop(event){
+    event.preventDefault();
+    var data=event.dataTransfer.getData("text");
+    event.currentTarget.appendChild(document.getElementById(data));
+}
